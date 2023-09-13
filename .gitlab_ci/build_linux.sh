9d0edbfc41d40fab2c429c3ee24fa8f5240a1dcf
yl1720 <2023wxy>
2023-09-13 12:53:33 +0800

    【feat】CI/CD build不写入版本号

--- .gitlab_ci/build_linux.sh
+++ .gitlab_ci/build_linux.sh
@@ -4,7 +4,7 @@ set -x
 echo "############## Begin to execute job:build ##############"
 #<================ user modify(please modify content of script)
 mkdir -p $TGZ_PACKAGE_PATH_LINUX/chatgpt-next-web
-sed -i "1c APP_VERSION = ${CI_COMMIT_TAG-:${CI_COMMIT_SHORT_SHA}}" .env.local
+#sed -i "1c APP_VERSION = ${CI_COMMIT_TAG-:${CI_COMMIT_SHORT_SHA}}" .env.local
 
 npm config set strict-ssl false
 npm install --registry=https://registry.npm.taobao.org/
